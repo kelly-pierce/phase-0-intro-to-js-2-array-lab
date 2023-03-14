229051b364fe160270a48d0b9193c36e05da25a7
Kelly <kelly@truenorth.us>
2023-03-13 22:44:53 -0400

    arrays practice lab finished

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+// /*
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const appendCat = [...cats, name];
+    return appendCat;
+}
+function prependCat(name){
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+function removeLastCat(){
+    const removeLastCat = cats.slice(0, -1);
+    return removeLastCat;
+}
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
